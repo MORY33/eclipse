38f990fd12584bfef4a563dded0abd640dd5b109
MORY33 <rafa.karwot@gmail.com>
2022-06-11 21:50:37 +0200

    cleaning a little bit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Images from "./components/Images";
-import Footer from "./components/Footer";
 import { Box, CircularProgress, Typography } from "@mui/material"
 import "./App.css"
-// import records from "./data.json"
 
 export default function App() {
- const [data, setData] = useState([]);
   const [images, setImages] = useState([]);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(true);
@@ -15,12 +12,6 @@ export default function App() {
   useEffect(() => {
     const baseUrl = "https://api.unsplash.com";
     const amount = 26;
-    // const size = "/100x100"
-    // fit=crop&w=300&h=300
-    // const accessKey = process.env.REACT_APP_ACCESSKEY;
-    // https://api.unsplash.com/photos/random?client_id=T-1KTKJTKQx0OiYsjG1dcZ1JwihrJfrRhog7SmfhEeU&count=10
-
-    // ${baseUrl}/photos/random?client_id=T-1KTKJTKQx0OiYsjG1dcZ1JwihrJfrRhog7SmfhEeU&count=10
 
 
     axios
@@ -58,6 +49,5 @@ export default function App() {
    {images.map(image=> (
      <Images url={image.urls.full} element={image.user.username} date = {image.created_at} key={image.id}/>
    ))}
-   {/* <Footer/> */}
   </div>;
 }
