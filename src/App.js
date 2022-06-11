d6ddd4710c77d24ce1aa65b5e2ad107bb8ba58eb
MORY33 <rafa.karwot@gmail.com>
2022-06-11 20:55:01 +0200

    added styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Images from "./components/Images";
+import Footer from "./components/Footer";
+import "./App.css"
+// import records from "./data.json"
 
 export default function App() {
  const [data, setData] = useState([]);
@@ -10,25 +13,41 @@ export default function App() {
 
   useEffect(() => {
     const baseUrl = "https://api.unsplash.com";
-    const width = ""
+    const amount = 26;
+    // const size = "/100x100"
+    // fit=crop&w=300&h=300
     // const accessKey = process.env.REACT_APP_ACCESSKEY;
-
     // https://api.unsplash.com/photos/random?client_id=T-1KTKJTKQx0OiYsjG1dcZ1JwihrJfrRhog7SmfhEeU&count=10
 
+    // ${baseUrl}/photos/random?client_id=T-1KTKJTKQx0OiYsjG1dcZ1JwihrJfrRhog7SmfhEeU&count=10
+
+
     axios
-      .get(`${baseUrl}/photos/random?client_id=T-1KTKJTKQx0OiYsjG1dcZ1JwihrJfrRhog7SmfhEeU&count=10`)
+      .get(`${baseUrl}/photos/?client_id=T-1KTKJTKQx0OiYsjG1dcZ1JwihrJfrRhog7SmfhEeU&count=${amount}`)
       .then(response => setImages([...images,...response.data]))
-      // .catch(err => setError(err))
-      // .finally(()=> setLoading(false))
+      .catch(err => setError(err))
+      .finally(()=> setLoading(false))
+
 
   },[])
 
 
 
- return <div className="App">App
-   <Images/>
+
+
+  // records.map(record => {
+  //   console.log(records)
+  // })
+
+
+
+
+
+
+ return <div className="App">
    {images.map(image=> (
-     <Images url={image.urls.thumb} element={image.user.username} date = {image.created_at} key={image.id}/>
+     <Images url={image.urls.full} element={image.user.username} date = {image.created_at} key={image.id}/>
    ))}
+   <Footer/>
   </div>;
 }
