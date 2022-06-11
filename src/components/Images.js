d50e16c442558df4828ee858306188b5e88a365d
MORY33 <rafa.karwot@gmail.com>
2022-06-11 21:51:07 +0200

    final version

--- src/components/Images.js
+++ src/components/Images.js
@@ -3,17 +3,13 @@ import { Fragment } from 'react'
 import Header from 'react'
 const Images = ({element, url, date}) => {
 
-// const dateTime = `${date.getDate()} + ${date.getFullYear()}`;
-// console.log(`${date.getDate()}`) 
+
 
 let year = date.slice(0,4);
 let month = date.slice(5,7);
 let day = date.slice(8,10)
-// console.log(day);
-// console.log(year);
-// console.log(month);
+
 let newDate = day + "/" + month + "/" + year;
-// console.log(newDate);
 
 
 if (element!=null){
@@ -26,7 +22,6 @@ if (element!=null){
                 <div>
                     <p className='nick'> {element}   </p>
                     <p className='super'>  on</p>
-                    {/* <p> {date.slice(0,10)}</p> */}
                     <p>{newDate}</p>
                 </div>
             </div>
